0ec1a5dafc008ddf8c5aca248053a7fce872a6d7
Nekylaw <nekylaw.ao@gmail.com>
2025-02-17 00:22:32 +0100

    snapping to receptacle

--- Assets/Scripts/ReceptacleManager.cs
+++ Assets/Scripts/ReceptacleManager.cs
@@ -1,31 +1,56 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ReceptacleManager : MonoBehaviour
-{
+{   [SerializeField] Rigidbody orb_Rb; 
+    
 
-        public void OnTriggerEnter(Collider other)
+    float snappingRange = 5f; // allowed range for snapping orb to receptacle
+    float snappingForce = 5f; // force that is gonna pull the orb
+
+    void Update()
+    {
+        SnapToReceptacle();
+        UpdateSnapDirection();
+    }
+
+    void SnapToReceptacle()
+    {
+        float Distance = Vector3.Distance(orb_Rb.transform.position, this.transform.position);
+
+        if (Distance < snappingRange) // orb is in the range of the snapping area
         {
-            Debug.Log("ahh");
-            if (other.gameObject.tag == "Orb")
-            {
-                Renderer myRenderer = GetComponent<Renderer>();
-                if (myRenderer != null)
-                {
-                    if (myRenderer.material.color == Color.red)
-                    {
-                        myRenderer.material.color = Color.green;
-                    }
-                    else
-                    {
-                        myRenderer.material.color = Color.red;
-                    }
-                }
-                Debug.Log("Player Collided with CUBE");
-            }
-            else
-            {
-                Debug.Log("CUBE trigger called by non-player");
-            }
+            float distanceToSnap = Mathf.InverseLerp(snappingRange, 0f, Distance); 
+            float strength = Mathf.Lerp(0f, snappingForce, distanceToSnap); 
+            Vector3 directionToReceptacle = (this.transform.position - orb_Rb.transform.position).normalized; 
+
+            orb_Rb.AddForce(directionToReceptacle * strength, ForceMode.Force);// apply force to the orb 
+
         }
+    }
+
+    void UpdateSnapDirection()
+    {
+        Vector3 receptacleDir = (orb_Rb.transform.position - transform.position).normalized; // direction to the receptacle
+        transform.forward = receptacleDir; // make the orb go to that direction 
+    }
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ahh"); 
+        if (other.gameObject.tag == "Orb")
+        {
+            Renderer myRenderer = GetComponent<Renderer>();
+            if (myRenderer != null)
+            {
+                myRenderer.material.color = Color.red;
+            } 
+            Debug.Log("Orb collided w receptacle !!!!!");
+        }
+        
+    }
+
+
+        
     
 }
