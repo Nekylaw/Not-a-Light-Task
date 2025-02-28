345f33a7b4e0dbda2b2891ea54e30678d2616fde
Adam Adhar <adamadhar28@gmail.com>
2025-02-28 03:04:28 +0100

    # Improve movements

--- Assets/_System/Behaviors/Movement/MovementBehaviorComponent.cs
+++ Assets/_System/Behaviors/Movement/MovementBehaviorComponent.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json.Bson;
 using UnityEngine;
 
 public class MovementBehaviorComponent : MonoBehaviour
@@ -7,13 +8,15 @@ public class MovementBehaviorComponent : MonoBehaviour
     private MovementSettings _settings = null;
 
     private Rigidbody _rigidbody = null;
-
-    private bool _isAiming = false;
+    private DetectionBehaviorComponent _detector = null;
 
     private void Awake()
     {
         if (!TryGetComponent<Rigidbody>(out _rigidbody))
             Debug.LogError($"{nameof(Rigidbody)} component not found.", this);
+
+        if (!TryGetComponent<DetectionBehaviorComponent>(out _detector))
+            Debug.LogError($"{nameof(DetectionBehaviorComponent)} component not found.", this);
     }
 
     void Start()
@@ -22,17 +25,41 @@ public class MovementBehaviorComponent : MonoBehaviour
             Debug.LogError("Settings not found", this);
     }
 
+    private void FixedUpdate()
+    {
+        HandleFalling();
+        AdjustPosition();
+    }
 
     public bool Move(Vector3 direction, float delta, bool isAiming)
     {
         direction = _settings.UseProgressiveMove ? Vector3.ClampMagnitude(direction, 1) : direction.normalized;
-
-        _isAiming = isAiming;
-
         float speed = isAiming ? _settings.Speed * _settings.SpeedRatioOnAim : _settings.Speed;
 
-        _rigidbody.linearVelocity = new Vector3(direction.x, _rigidbody.linearVelocity.y,direction.z) * speed;
+        _rigidbody.linearVelocity = new Vector3(direction.x * speed, _rigidbody.linearVelocity.y, direction.z * speed);
 
         return true;
     }
+
+    private void AdjustPosition()
+    {
+
+    }
+
+    private void HandleFalling()
+    {
+        if (_detector.IsGrounded)
+            return;
+
+        _rigidbody.AddForce(_settings.GravityMultiplier * Physics.gravity, ForceMode.Acceleration);
+
+        _rigidbody.linearVelocity = new Vector3
+            (
+            _rigidbody.linearVelocity.x * _settings.AirControl,
+            Mathf.Max(_rigidbody.linearVelocity.y, -_settings.FallingSpeed),
+            _rigidbody.linearVelocity.z * _settings.AirControl
+            );
+
+    }
+
 }
