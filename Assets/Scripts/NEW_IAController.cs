35a141c926dd04f41ac529d52737614aa37e9cf9
Nekylaw <nekylaw.ao@gmail.com>
2025-05-08 18:03:28 +0200

    creature pacify

--- Assets/Scripts/NEW_IAController.cs
+++ Assets/Scripts/NEW_IAController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
@@ -9,35 +10,52 @@ using Random = UnityEngine.Random;
 public class NEW_IAController : MonoBehaviour
 {
    public GameObject[] allOrbs;
-   public GameObject nearestObject;
+   public GameObject nearestObject; 
+   public List<GameObject> orbsEaten;
    float distance;
    float nearestDistance = 100;
 
+   public bool isEvil = true;
    void Update()
    {
-      
-      if (nearestObject != null)
+      if (nearestObject != null && isEvil == true)
       {
          MoveTo(nearestObject.transform.position);
       }
       else
       {
-         ScanWorldOrbs();
+         if (isEvil == true)
+         {
+            ScanWorldOrbs();
+         }
          WanderAround();
       }
-      
    }
 
    private void OnCollisionEnter(Collision other)
    {
-      if (other.gameObject.CompareTag("Orb"))
+      if (other.gameObject.CompareTag("Orb") && isEvil == true)
       {
          other.gameObject.SetActive(false);
-         Debug.Log("orb eaten by: " + this.name);
          nearestObject = null;
+         Debug.Log("orb eaten by: " + this.name);
+         orbsEaten.Add(other.gameObject);
       }
-      
+
+      if (other.gameObject.CompareTag("Pacify"))
+      {
+         isEvil = false;
+         foreach (var orb in orbsEaten)
+         {
+            orb.SetActive(true);
+            orb.transform.position = this.transform.position;
+            Debug.Log("creature pacified : orb given back !"); 
+         }
+         orbsEaten.Clear();
+      }
    }
+   
+   
 
    #region ScanOrbs
 
