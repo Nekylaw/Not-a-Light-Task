3cd183ed18260a6249a421a964291fcc5cfd94a0
Nekylaw <nekylaw.ao@gmail.com>
2025-05-18 23:58:10 +0200

    add from pacify

---
+++ Assets/_System/IA/CreatureState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CreatureState : MonoBehaviour
+{ 
+    public GameObject body;
+    public bool isEvil = true;
+    public Material FriendlyMaterial;
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Pacify")
+        {
+            isEvil = false;
+            body.GetComponent<MeshRenderer>().material = FriendlyMaterial;
+        }
+    }
+}
+
+
+
---
+++ Assets/_System/IA/NEW_IAController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+
+public class NEW_IAController : MonoBehaviour
+{
+   public GameObject[] allOrbs;
+   public GameObject nearestObject; 
+   public List<GameObject> orbsEaten;
+   float distance;
+   float nearestDistance = 100;
+
+   private CreatureState creatureState = new CreatureState();
+   void Update()
+   {
+      if (nearestObject != null && creatureState.isEvil == true)
+      {
+         MoveTo(nearestObject.transform.position);
+      }
+      else
+      {
+         if (creatureState.isEvil == true)
+         {
+            ScanWorldOrbs();
+         }
+         WanderAround();
+      }
+   }
+
+   private void OnCollisionEnter(Collision other)
+   {
+      if (other.gameObject.CompareTag("Orb") && creatureState.isEvil)
+      {
+         other.gameObject.SetActive(false);
+         nearestObject = null;
+         Debug.Log("orb eaten by: " + this.name);
+         orbsEaten.Add(other.gameObject);
+      }
+
+      if (other.gameObject.CompareTag("Pacify"))
+      {
+         creatureState.isEvil = false;
+         foreach (var orb in orbsEaten)
+         {
+            orb.SetActive(true);
+            orb.transform.position = this.transform.position;
+            Debug.Log("creature pacified : orb given back !"); 
+         }
+         orbsEaten.Clear();
+      }
+   }
+   
+   
+
+   #region ScanOrbs
+
+   void ScanWorldOrbs()
+   {
+      allOrbs = GameObject.FindGameObjectsWithTag("Orb");
+       for (int i = 0; i < allOrbs.Length; i++)
+       {
+          distance = Vector3.Distance(this.transform.position , allOrbs[i].transform.position);
+          if (distance < nearestDistance)
+          {
+             nearestObject = allOrbs[i];
+             nearestDistance = distance;
+             Debug.Log("new nearest orb: " + nearestObject.name);
+          }
+       }
+   }
+   
+   
+
+   #endregion
+   
+   #region Wander
+   
+   [SerializeField] float circRadius = 10f;
+   [SerializeField] float circDistance = 10f;	
+   [FormerlySerializedAs("wanderJitter")] [SerializeField] float wanderRandomizer = 1f;
+
+   Vector3 aiWanderGoal = Vector3.zero;
+
+   void WanderAround() {
+
+      aiWanderGoal += new Vector3( Random.Range(-1.0f, 1.0f) * wanderRandomizer, 0f,
+         Random.Range(-1.0f, 1.0f) * wanderRandomizer );
+
+      aiWanderGoal.Normalize();
+      aiWanderGoal *= circRadius;
+
+      var locTarget = aiWanderGoal + new Vector3(0, 0, circDistance);
+      var worldCoord = gameObject.transform.InverseTransformVector(locTarget);
+
+      MoveTo(worldCoord);
+   }
+
+   void MoveTo( Vector3 location ) 
+   {
+      NavMeshAgent agent = GetComponent<NavMeshAgent>();
+      agent.SetDestination( location );
+   }
+   #endregion
+}
