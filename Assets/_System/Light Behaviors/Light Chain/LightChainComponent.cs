c11b4c8aa40c07957921ce18852013aa9b3834f1
Adam Adhar <adamadhar28@gmail.com>
2025-06-05 13:24:40 +0200

    #Update Light chain with modes

--- Assets/_System/Light Behaviors/Light Chain/LightChainComponent.cs
+++ Assets/_System/Light Behaviors/Light Chain/LightChainComponent.cs
@@ -1,13 +1,29 @@
+using System.Collections;
 using System.Linq;
-
 using UnityEngine;
 
 namespace Game.Services.LightSources
 {
     public class LightChainComponent : MonoBehaviour
     {
+        public enum LightDelayMode
+        {
+            Simultaneous,
+            Sequential,
+            Random
+        }
+
+        [Header("Light Chain Settings")]
         [SerializeField]
         private LightSourceComponent[] _lights = null;
+
+        [SerializeField]
+        private float _delayBetweenLights = 0.3f;
+
+        [SerializeField]
+        private LightDelayMode _lightMode = LightDelayMode.Sequential;
+
+        private Coroutine _chainRoutine;
 
         private void OnEnable()
         {
@@ -24,13 +40,54 @@ namespace Game.Services.LightSources
             if (!_lights.Contains(triggeredLight))
                 return;
 
-            foreach (LightSourceComponent light in _lights)
-            {
-                if (light.IsLightOn)
-                    continue;
+            if (_chainRoutine != null)
+                return;
 
-                LightSourcesService.Instance.SwitchOn(light);
-            }
+            _chainRoutine = StartCoroutine(ChainLightSequence(triggeredLight));
         }
+
+        private IEnumerator ChainLightSequence(LightSourceComponent triggeredLight)
+        {
+            var chainList = _lights
+                .Where(l => l != null && l != triggeredLight && !l.IsLightOn)
+                .ToList();
+
+            chainList.Insert(0, triggeredLight);
+
+            switch (_lightMode)
+            {
+                case LightDelayMode.Simultaneous:
+                    foreach (var light in chainList)
+                    {
+                        LightSourcesService.Instance.SwitchOn(light);
+                    }
+                    break;
+
+                case LightDelayMode.Sequential:
+                    var sequentialOrder = chainList
+                        .OrderBy(l => l == triggeredLight ? -1 : Vector3.Distance(triggeredLight.transform.position, l.transform.position))
+                        .ToList();
+
+                    foreach (var light in sequentialOrder)
+                    {
+                        LightSourcesService.Instance.SwitchOn(light);
+                        yield return new WaitForSeconds(_delayBetweenLights);
+                    }
+                    break;
+
+                case LightDelayMode.Random:
+                    var randomList = chainList.OrderBy(_ => Random.value).ToList();
+
+                    foreach (var light in randomList)
+                    {
+                        LightSourcesService.Instance.SwitchOn(light);
+                        yield return new WaitForSeconds(_delayBetweenLights);
+                    }
+                    break;
+            }
+
+            _chainRoutine = null;
+        }
+
     }
 }
