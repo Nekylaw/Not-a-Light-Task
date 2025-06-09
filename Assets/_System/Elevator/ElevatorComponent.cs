6be3f725f0afe22978a96a774f486c993129c1ef
Adam Adhar <adamadhar28@gmail.com>
2025-06-09 17:58:03 +0200

    + Elevator

---
+++ Assets/_System/Elevator/ElevatorComponent.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using UnityEngine;
+
+public class ElevatorComponent : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _topPoint = null;
+    [SerializeField]
+    private Transform _bottomPoint = null;
+    [SerializeField]
+    private float _duration = 2f;
+
+    [SerializeField]
+    private AnimationCurve _animationCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+    private bool _isActivated = false;
+    private bool _isTop = false;
+
+    private Vector3 _bottomPos = Vector3.zero;
+    private Vector3 _topPos = Vector3.zero;
+
+    private void Awake()
+    {
+        transform.position = _bottomPoint.position;
+        _isActivated = false;
+        _isTop = false;
+
+        _bottomPos = _bottomPoint.position;
+        _topPos = _topPoint.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_isActivated || !other.gameObject.TryGetComponent<PlayerController>(out var player))
+            return;
+
+        if (player.transform.parent != transform)
+            player.transform.SetParent(transform);
+
+        ActivateElevator();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.TryGetComponent<PlayerController>(out var player))
+            return;
+
+        if (player.transform.parent == transform)
+            player.transform.SetParent(null);
+    }
+
+    private void ActivateElevator()
+    {
+        if (_isActivated)
+            return;
+
+        _isActivated = true;
+        Transform target = _isTop ? _bottomPoint : _topPoint;
+        StartCoroutine(AnimateElevatorCoroutine(target));
+    }
+
+    private IEnumerator AnimateElevatorCoroutine(Transform target)
+    {
+        float elapsedTime = 0f;
+
+        Vector3 targetPos = target == _topPoint ? _topPos : _bottomPos; 
+        Vector3 startPos = transform.position;
+
+        while (Vector3.Distance(transform.position, targetPos) > 0.1f)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float t = Mathf.Clamp01(elapsedTime / _duration);
+
+            transform.position = Vector3.Lerp(startPos, targetPos, _animationCurve.Evaluate(t));
+            yield return null;
+        }
+
+        transform.position = targetPos;
+        _isTop = !_isTop;
+        _isActivated = false;
+
+        Debug.Log("Elevator reached target position.");
+    }
+}
