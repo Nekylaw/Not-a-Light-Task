e0295609b6256146d0436003ab74f6288c5e766b
RenSunB0y <150889605+RenSunB0y@users.noreply.github.com>
2025-06-17 04:54:31 +0200

    +ouverture du grillage

--- Assets/GrillageBehaviour.cs
+++ Assets/GrillageBehaviour.cs
@@ -1,12 +1,30 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class GrillageBehaviour : MonoBehaviour
 {
     [SerializeField] private Transform pivot;
+    
+    [SerializeField] private Transform panelDroit;
+    [SerializeField] private Transform panelGauche;
+    [SerializeField] private Transform charniereDroite;
+    [SerializeField] private Transform charniereGauche;
+
+    [SerializeField] private float duration;
 
     
-    public void OpenGate()
+    public void OpenGateRotateAround()
     {
         transform.RotateAround(pivot.position, Vector3.up, 150f);
+    }
+
+    public void OpenGateProperly()
+    {
+        Vector3 vecDroit = new Vector3(0, 0, 70);
+        Vector3 vecGauche = new Vector3(0, 0, -70);
+        panelDroit.DOLocalRotate(vecDroit, duration, RotateMode.LocalAxisAdd);
+        panelGauche.DOLocalRotate(vecGauche, duration, RotateMode.LocalAxisAdd);
+        charniereDroite.DOLocalRotate(vecDroit, duration, RotateMode.LocalAxisAdd);
+        charniereGauche.DOLocalRotate(vecGauche , duration, RotateMode.LocalAxisAdd);
     }
 }
--- Assets/_System/Managers/EndLevelManager.cs
+++ Assets/_System/Managers/EndLevelManager.cs
@@ -42,9 +42,7 @@ namespace _System.Game_Manager
         
         #region Serialize Fields
 
-        [SerializeField] private GameObject firstObjectEnlighten;
         [SerializeField] private GameObject firstCpToOpen;
-        [SerializeField] private GameObject secondObjectEnlighten;
         [SerializeField] private GameObject secondCpToOpen;
         //...
         
@@ -106,15 +104,16 @@ namespace _System.Game_Manager
 
         private void ListsLoader()
         {
+            Debug.Log(LightSourcesService.Instance.LightSources.Length);
             foreach (var VARIABLE in LightSourcesService.Instance.LightSources)
             {
-                if (VARIABLE.LightGroupId == 0)
+                Debug.Log(VARIABLE.name);
+                if (VARIABLE.LightGroupId != 0)
                 {
-                    Debug.Log("light source sans ID : " + VARIABLE.gameObject.name);
-                    return;
+                    SortLightSources(VARIABLE.LightGroupId, VARIABLE);
                 }
                 
-                SortLightSources(VARIABLE.LightGroupId, VARIABLE);
+                Debug.Log("light source sans ID : " + VARIABLE.gameObject.name);
             }
         }
 
@@ -142,6 +141,7 @@ namespace _System.Game_Manager
         
         private bool CheckingEachSource(List<LightSourceComponent> lightSources)
         {
+            Debug.Log("checking");
             foreach (var VARIABLE in lightSources)
             {
                 if (!VARIABLE.IsLightOn)
@@ -160,11 +160,7 @@ namespace _System.Game_Manager
 
         private void FirstCheckPoint()
         {
-            var newMat = new Material(Shader.Find("Unlit/Color"));
-            newMat.SetColor(Color1, Color.green);
-            firstObjectEnlighten.GetComponent<MeshRenderer>().material = newMat;
-            Destroy(firstCpToOpen);
-            Debug.Log("First Check Point : Activation");
+            firstCpToOpen.GetComponent<GrillageBehaviour>().OpenGateProperly();
         }
 
         private void SecondCheckPoint()
