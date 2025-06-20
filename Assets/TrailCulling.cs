da35f40bb116db134230b684d81bff0e6a054153
RenSunB0y <150889605+RenSunB0y@users.noreply.github.com>
2025-06-20 22:29:17 +0200

    +trail culling

---
+++ Assets/TrailCulling.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class TrailCulling : MonoBehaviour,ICullable
+{
+    #region Cull
+
+    public ICullable.ECullMode CullMode { get; set; }
+    public float CullRange { get; set; }
+    public int CullableIndex { get; set; }
+
+    private VisualEffect VFX;
+    private TrailRenderer TrailRenderer;
+    
+    private void Start()
+    {
+        CullMode = ICullable.ECullMode.Frustum;
+        VFX = GetComponentInChildren<VisualEffect>();
+        TrailRenderer = GetComponentInChildren<TrailRenderer>();
+    }
+
+    private bool _isCulled;
+    public void OnBecomeVisible()
+    {
+        _isCulled = false;
+        VFX.enabled = true;
+        TrailRenderer.enabled = true;
+    }
+
+    public void OnBecomeInvisible()
+    {
+        _isCulled = true;
+
+        Debug.Log($"{name} has been culled");
+        
+        VFX.enabled = false;
+        TrailRenderer.enabled = false;
+    }
+
+    public Vector3 GetCullPosition()
+    {
+        return transform.position;
+    }
+
+    public float GetCullRadius()
+    {
+        return CullRange;
+    }
+
+    #endregion
+}
